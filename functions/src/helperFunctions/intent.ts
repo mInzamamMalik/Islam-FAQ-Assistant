b1f8da86cf2a5d2bb2e228835d4aabbaba016976
daniyalnagori <daniyalnagori@yahoo.com>
2018-01-29 21:39:14 +0500

    add intent file

---
+++ functions/src/helperFunctions/intent.ts
@@ -0,0 +1,21 @@
+import * as functions from 'firebase-functions'
+import * as request from 'request'
+let access_token = "Bearer 3065ceaaf2f24c1cb51dbad1987b1a01"
+
+export interface userSay {
+    "count": number,
+    "data": object[]
+}
+
+export interface response {
+    action:string,
+    affectedContexts:any[],
+    defaultResponsePlatforms:Object,
+    messages: Object[],
+    parameters:Object[],
+    resetContexts:boolean
+}
+
+export function createIntent(name:string, userSays:userSay[],responses:response[], events:any[], fallbackIntent:boolean = false, webhookForSlotFilling:boolean = false, webhookUsed:boolean = false, contexts:string[] = [] ) {
+
+}
